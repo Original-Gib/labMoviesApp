96db9a6b5939d3b49d7fdba1288226c1a6485134
Adam Gibson <adamhenrygibson@gmail.com>
2023-06-25 16:45:18 +0100

    updated favourite icon logic in headerMovie

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import Avatar from "@mui/material/Avatar";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import { CardHeader } from "@mui/material";
 
 const styles = {
     root: {
@@ -16,10 +17,40 @@ const styles = {
         flexWrap: "wrap",
         padding: 1.5,
     },
+    favouriteAvatar: {
+        backgroundColor: "rgb(255, 0, 0)",
+    },
+    nonFavouriteAvatar: {
+        backgroundColor: "rgb(220, 220, 220)",
+    },
 };
 
 const MovieHeader = (props) => {
     const movie = props.movie;
+    const favouriteMovies = JSON.parse(localStorage.getItem("favourites"));
+    let favouriteIcon = null;
+    if (favouriteMovies.length > 0) {
+        favouriteMovies.forEach(element => {
+            if (movie.id == element.id && element.favourite == true) {
+                favouriteIcon = <Avatar sx={styles.favouriteAvatar}>
+                    <FavoriteIcon />
+                </Avatar>
+            } else {
+                favouriteIcon = <Avatar sx={styles.nonFavouriteAvatar}>
+                    <FavoriteIcon />
+                </Avatar>
+            }
+        })
+    } else {
+        favouriteIcon = <Avatar sx={styles.nonFavouriteAvatar}>
+            <FavoriteIcon />
+        </Avatar>
+    };
+
+
+
+
+
 
     return (
         <Paper component="div" sx={styles.root}>
@@ -27,9 +58,7 @@ const MovieHeader = (props) => {
                 <ArrowBackIcon color="primary" fontSize="large" />
             </IconButton>
 
-            <Avatar sx={styles.avatar}>
-                <FavoriteIcon />
-            </Avatar>
+            {favouriteIcon}
 
             <Typography variant="h4" component="h3">
                 {movie.title}{"   "}
