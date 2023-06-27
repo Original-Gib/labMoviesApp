ef70f9d472dc7be601ad75a72a6d7cc263305642
Adam Gibson <adamhenrygibson@gmail.com>
2023-06-27 23:07:11 +0100

    Added upcoming movie page

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,9 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingPage from "./pages/upcomingMoviesPage";
+
 
 
 
@@ -16,6 +18,7 @@ const App = () => {
             <Routes>
                 <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
                 <Route path="/movies/:id" element={<MoviePage />} />
+                <Route path="/movies/upcoming" element={<UpcomingPage />} />
                 <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<Navigate to="/" />} />
                 <Route path="/reviews/:id" element={<MovieReviewPage />} />
