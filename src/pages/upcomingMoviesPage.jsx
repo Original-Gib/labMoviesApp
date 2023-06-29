a0c1b0cdf9c4ecd65d825996cd9f4eae034a4c2d
Adam Gibson <adamhenrygibson@gmail.com>
2023-06-29 20:26:17 +0100

    New button for Upcoming movies page.

--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToPlaylistIcon from "../components/cardIcons/addToPlaylist";
 
 
 const UpcomingPage = (props) => {
@@ -22,7 +22,7 @@ const UpcomingPage = (props) => {
             title='Upcoming Movies'
             movies={movies}
             action={(movie) => {
-                return <AddToFavouritesIcon movie={movie} />
+                return <AddToPlaylistIcon movie={movie} />
             }}
         />
     );
