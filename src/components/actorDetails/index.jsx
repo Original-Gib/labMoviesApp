97238b8074fbc67ec51a301124f37644574a5652
Adam Gibson <adamhenrygibson@gmail.com>
2023-07-23 17:43:17 +0100

    Added actor detail page

---
+++ src/components/actorDetails/index.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Paper from "@mui/material/Paper";
+import CakeIcon from '@mui/icons-material/Cake';
+import PinDropIcon from '@mui/icons-material/PinDrop';
+import StarRate from "@mui/icons-material/StarRate";
+import Typography from "@mui/material/Typography";
+
+import NavigationIcon from "@mui/icons-material/Navigation";
+import Fab from "@mui/material/Fab";
+
+
+const styles = {
+    chipSet: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        listStyle: "none",
+        padding: 1.5,
+        margin: 0,
+    },
+    chipLabel: {
+        margin: 0.5,
+    },
+    fab: {
+        position: "fixed",
+        top: 50,
+        right: 2,
+    },
+};
+
+const ActorDetails = ({ actor }) => {
+    const [drawerOpen, setDrawerOpen] = useState(false); // New
+
+    return (
+        <>
+            <Typography variant="h5" component="h3">
+                Overview
+            </Typography>
+
+            <Typography variant="h6" component="p">
+                {actor.biography}
+            </Typography>
+
+            <Paper component="ul" sx={styles.chipSet}>
+                <Chip icon={<CakeIcon />} label={`${actor.birthday}`} />
+                <Chip
+                    icon={<PinDropIcon />}
+                    label={`${actor.place_of_birth}`}
+                />
+                <Chip
+                    icon={<StarRate />}
+                    label={`${actor.popularity}`}
+                />
+
+            </Paper>
+            <Fab
+                color="secondary"
+                variant="extended"
+                onClick={() => setDrawerOpen(true)}
+                sx={styles.fab}
+            >
+                <NavigationIcon />
+                Reviews
+            </Fab>
+
+        </>
+    );
+};
+export default ActorDetails;
--- src/index.jsx
+++ src/index.jsx
@@ -15,6 +15,7 @@ import { ReactQueryDevtools } from 'react-query/devtools'
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import ActorPage from "./pages/actors";
+import ActorDetailsPage from "./pages/actorDetailsPage";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -46,6 +47,7 @@ const App = () => {
                         <Route path="*" element={<Navigate to="/" />} />
                         <Route path="/reviews/:id" element={<MovieReviewPage />} />
                         <Route path="/actors" element={<ActorPage />} />
+                        <Route path="/actors/:id" element={<ActorDetailsPage />} />
                     </Routes>
                 </MoviesContextProvider>
             </BrowserRouter>
